f9f934d8007437889b1cf71137f940390425501b
bjoern <bjoern@10df6cd2-1211-11de-9cb5-a363f76c21d8>
2007-03-07 13:00:43 +0000

    initial import
    git-svn-id: svn+ssh://thinkforge.org/svnroot/dokuwiki/trunk@2 10df6cd2-1211-11de-9cb5-a363f76c21d8

---
+++ dokuwiki.inc.php
@@ -0,0 +1,20 @@
+<?php
+$__olddir = getcwd();
+chdir(dirname(__FILE__));
+if(!defined('DOKU_INC')) define('DOKU_INC',realpath(dirname(__FILE__)).'/');
+  require_once(DOKU_INC.'inc/init.php');
+  require_once(DOKU_INC.'inc/common.php');
+  require_once(DOKU_INC.'inc/events.php');
+  require_once(DOKU_INC.'inc/pageutils.php');
+  require_once(DOKU_INC.'inc/html.php');
+  require_once(DOKU_INC.'inc/auth.php');
+  require_once(DOKU_INC.'inc/actions.php');
+
+  ob_start();
+  tpl_metaheaders(false);
+  $__headers = ob_get_contents();
+  ob_end_clean();
+  
+  $GLOBALS['he_add'][] = $__headers;
+  
+chdir($__olddir);
---
+++ lib/tpl/default/main.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * DokuWiki Default Template
+ *
+ * This is the template you need to change for the overall look
+ * of DokuWiki.
+ *
+ * You should leave the doctype at the very top - It should
+ * always be the very first line of a document.
+ *
+ * @link   http://wiki.splitbrain.org/wiki:tpl:templates
+ * @author Andreas Gohr <andi@splitbrain.org>
+ */
+
+// must be run from within DokuWiki
+if (!defined('DOKU_INC')) die();
+
+/*
+ tpl_pagetitle();
+ echo "[" .strip_tags($conf['title']) . "]";
+*/
+?>
+<br />
+<div class="dokuwiki">
+  <?php html_msgarea()?>
+
+  <div class="stylehead">
+
+    <div class="header">
+      <div class="pagename">
+        [[<?php tpl_link(wl($ID,'do=backlink'),tpl_pagetitle($ID,true))?>]]
+      </div>
+      <div class="logo">
+        <?php tpl_link(wl(),$conf['title'],'name="dokuwiki__top" id="dokuwiki__top" accesskey="h" title="[ALT+H]"')?>
+      </div>
+
+      <div class="clearer"></div>
+    </div>
+
+    <?php /*old includehook*/ @include(dirname(__FILE__).'/header.html')?>
+
+    <div class="bar" id="bar__top">
+      <div class="bar-left" id="bar__topleft">
+        <?php tpl_button('edit')?>
+        <?php tpl_button('history')?>
+      </div>
+
+      <div class="bar-right" id="bar__topright">
+        <?php tpl_button('recent')?>
+        <?php tpl_searchform()?>&nbsp;
+      </div>
+
+      <div class="clearer"></div>
+    </div>
+
+    <?php if($conf['breadcrumbs']){?>
+    <div class="breadcrumbs">
+      <?php tpl_breadcrumbs()?>
+      <?php //tpl_youarehere() //(some people prefer this)?>
+    </div>
+    <?php }?>
+
+    <?php if($conf['youarehere']){?>
+    <div class="breadcrumbs">
+      <?php tpl_youarehere() ?>
+    </div>
+    <?php }?>
+
+  </div>
+  <?php flush()?>
+
+  <?php /*old includehook*/ @include(dirname(__FILE__).'/pageheader.html')?>
+
+  <div class="page">
+    <!-- wikipage start -->
+    <?php tpl_content()?>
+    <!-- wikipage stop -->
+  </div>
+
+  <div class="clearer">&nbsp;</div>
+
+  <?php flush()?>
+
+  <div class="stylefoot">
+
+    <div class="meta">
+      <div class="user">
+        <?php tpl_userinfo()?>
+      </div>
+      <div class="doc">
+        <?php tpl_pageinfo()?>
+      </div>
+    </div>
+
+   <?php /*old includehook*/ @include(dirname(__FILE__).'/pagefooter.html')?>
+
+    <div class="bar" id="bar__bottom">
+      <div class="bar-left" id="bar__bottomleft">
+        <?php tpl_button('edit')?>
+        <?php tpl_button('history')?>
+      </div>
+      <div class="bar-right" id="bar__bottomright">
+        <?php tpl_button('subscription')?>
+        <?php tpl_button('admin')?>
+        <?php tpl_button('profile')?>
+        <?php tpl_button('login')?>
+        <?php tpl_button('index')?>
+        <?php tpl_button('top')?>&nbsp;
+      </div>
+      <div class="clearer"></div>
+    </div>
+
+  </div>
+
+</div>
